96085b5031407bf914d0aea8c2fbcaecb280e1d7
VisedFeline121 <30751326+VisedFeline121@users.noreply.github.com>
2018-04-17 01:27:08 +0300

    Update honeypot.py

--- honeypot.py
+++ honeypot.py
@@ -4,15 +4,18 @@ import subprocess
 import os
 import socket
 import random
-import netuse
+#import netuse
 
 
 def map_drive(path):
-    netuse.mapNetDrive(path)
+    path = "net use " + path
+    os.system(path)
    
 
 def remove_drive(path):
-    netuse.removeNetDrive(path)
+    path = "net use " + path + " /delete"
+    os.system(path)
+    #netuse.removeNetDrive(path)
 
 
 def initiate(ip, port):
@@ -26,20 +29,25 @@ def initiate(ip, port):
 def handle_commands(command):
     logging.basicConfig(filename='msgs.txt', filemode='w', level=logging.DEBUG)
     logging.info(command)
-    os.system(command)
+    print os.system(command)
+    #os.system(command)
     #print command.split(" ")
     #print subprocess.check_output([command.split(" ")])
 
 
 def main():
-    ip = '127.0.0.1' #raw_input("insert ip: ")
+    """ip = '127.0.0.1' #raw_input("insert ip: ")
     port = random.randint(30000, 60000)
     server_socket = initiate('0.0.0.0', 50000)
     command = ''
     while command != 'exit':
         print os.getcwd() + ': ',
         command = server_socket.recv(1024)
+        handle_commands(command)"""
+    command = raw_input(os.getcwd() + ": ")
+    while command != 'exit':
         handle_commands(command)
+        command = raw_input(os.getcwd() + ": ")
 
 
 if __name__ == '__main__':
